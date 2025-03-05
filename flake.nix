b73f6b8df5d34de57c0003fa96d8387540616751
Stewart <andrew@desktop-knvu2bv.>
2025-03-05 12:17:59 -0500

    added custom preferences to Rstudio

--- flake.nix
+++ flake.nix
@@ -8,8 +8,8 @@
 
   outputs = { self , nixpkgs ,... }: let
     # system should match the system you are running on
-    # system = "x86_64-linux";
-    system = "aarch64-darwin";
+     system = "x86_64-linux";
+    #system = "aarch64-darwin";
   in {
     devShells."${system}".default = let
       pkgs = import nixpkgs {
@@ -18,16 +18,19 @@
     in pkgs.mkShell {
       # create an environment with required R Packages
       packages = with pkgs; [
+	(rWrapper.override{ packages = [rPackages.rstudio_prefs];})
         curlFull
         rPackages.curl
         (rstudioWrapper.override{ 
-          packages = with rPackages; [ tidyverse drc ]; # add new R packages here to get tied in
+          packages = with rPackages; [ tidyverse drc rstudio_prefs]; # add new R packages (from nix) here to get tied in
           }
         )
       ];
 
       shellHook = ''
+	R -e "require(rstudio.prefs); rstudio_config_path('./rstudio-prefs.json');"
         echo "RStudio Analysis Shell - ${system}"
+	rstudio
       '';
     };
   };
